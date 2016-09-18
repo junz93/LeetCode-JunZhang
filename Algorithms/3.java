79d2145e8c637e41f0c3c0d4cd5f549a64c7d2ec
Jun Zhang <mpgszhyh@gmail.com>
2016-09-17 18:20:04 -0700

    Update 3.java

--- Algorithms/3.java
+++ Algorithms/3.java
@@ -1,3 +1,8 @@
+/**
+ * 3. Longest Substring Without Repeating Characters
+ * Given a string, find the length of the longest substring without repeating characters.
+ */
+
 public class Solution {
     public static int lengthOfLongestSubstring(String s)
     {
