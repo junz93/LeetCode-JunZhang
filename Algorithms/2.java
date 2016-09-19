e34d6cb328387829ebd27561062c04c5798f6fb5
Jun Zhang <mpgszhyh@gmail.com>
2016-09-18 19:21:28 -0700

    Update 2.java

--- Algorithms/2.java
+++ Algorithms/2.java
@@ -12,6 +12,7 @@ class ListNode {
     int val;
     ListNode next;
     ListNode(int x) { val = x; }
+}
  
 public class Solution {
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2)
