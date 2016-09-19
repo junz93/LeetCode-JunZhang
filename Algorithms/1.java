94e9c6878c91072c0c310e1eb0f9c1a10dc5c060
Jun Zhang <mpgszhyh@gmail.com>
2016-09-18 17:44:37 -0700

    Update 1.java

--- Algorithms/1.java
+++ Algorithms/1.java
@@ -5,7 +5,25 @@
  */
  
 public class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        
+    public static int[] twoSum(int[] nums, int target)
+    {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> ht = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++)
+            ht.put(nums[i], i);
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(ht.containsKey(target - nums[i]))
+            {
+                int j = ht.get(target - nums[i]);
+                if(j != i)
+                {
+                    result[0] = i;
+                    result[1] = ht.get(target - nums[i]);
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
