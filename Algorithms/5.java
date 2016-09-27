e87f57048d3c9909bcb98abec0749450a86c6da5
Jun Zhang <mpgszhyh@gmail.com>
2016-09-26 20:13:44 -0700

    Update 5.java

--- Algorithms/5.java
+++ Algorithms/5.java
@@ -1,3 +1,8 @@
+/**
+ * 5. Longest Palindromic Substring
+ * Given a string S, find the longest palindromic substring in S.
+ * You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+ */
 public class Solution {
     public String longestPalindrome(String s) {
         int begin = 0, end = 0;
