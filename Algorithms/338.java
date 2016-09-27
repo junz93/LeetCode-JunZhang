fea26b68f4d41e2943a0b7e9c8b4057344c723bb
Jun Zhang <mpgszhyh@gmail.com>
2016-09-27 15:02:54 -0700

    Update 338.java

--- Algorithms/338.java
+++ Algorithms/338.java
@@ -1,1 +1,24 @@
+/**
+ * 338. Counting Bits
+ * Given a non negative integer number num.
+ * For every numbers i in the range 0 ≤ i ≤ num 
+ * calculate the number of 1's in their binary representation and return them as an array.
+ */
 
+public class Solution {
+    public int[] countBits(int num) {
+        int[] c = new int[num+1];
+        for(int i = 0; i <= num; i++)
+        {
+            int count = 0;
+            int n = i;
+            while(n > 0)
+            {
+                count += n%2;
+                n /= 2;
+            }
+            c[i] = count;
+        }
+        return c;
+    }
+}
