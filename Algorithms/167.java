a371d9910e5443ed363ae173984b96b84bc4d43a
Jun Zhang <mpgszhyh@gmail.com>
2016-10-05 14:09:04 -0700

    Update 167.java

--- Algorithms/167.java
+++ Algorithms/167.java
@@ -1,3 +1,12 @@
+/**
+ * 167. Two Sum II - Input array is sorted
+ * Given an array of integers that is already sorted in ascending order, 
+ * find two numbers such that they add up to a specific target number.
+ * The function twoSum should return indices of the two numbers such that they add up to the target.
+ * Note: your returned answers (both index1 and index2) are not zero-based.
+ * You may assume that each input would have exactly one solution.
+ */
+
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int[] result = new int[2];
