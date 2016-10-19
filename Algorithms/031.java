90d3ec6fe9318996d4dc84913e520ce335e25a7d
Jun Zhang <mpgszhyh@gmail.com>
2016-10-19 14:36:07 -0700

    Update 031.java

--- Algorithms/031.java
+++ Algorithms/031.java
@@ -13,13 +13,11 @@ public class Solution {
         for(i = nums.length-1; i>0 && nums[i]<=nums[i-1]; i--) ;
         if(i != 0)
         {
-            int k = i;
-            for(int j = i; j < nums.length; j++)
-                if(nums[j]>nums[i-1] && nums[j]<nums[k])
-                    k = j;
+            int j;
+            for(j = nums.length-1; j>=i && nums[j]<=nums[i-1]; j--) ;
             int temp = nums[i-1];
-            nums[i-1] = nums[k];
-            nums[k] = temp;            
+            nums[i-1] = nums[j];
+            nums[j] = temp;
         }
         Arrays.sort(nums, i, nums.length);
     }
@@ -30,13 +28,11 @@ public class Solution {
         for(i = nums.length-1; i>0 && nums[i]<=nums[i-1]; i--) ;
         if(i != 0)
         {
-            int k = i;
-            for(int j = i; j < nums.length; j++)
-                if(nums[j]>nums[i-1] && nums[j]<=nums[k])    // the '<=' sign here is crucial and cannot be replaced with '<' sign
-                    k = j;
+            int j;
+            for(j = nums.length-1; j>=i && nums[j]<=nums[i-1]; j--) ;
             int temp = nums[i-1];
-            nums[i-1] = nums[k];
-            nums[k] = temp;
+            nums[i-1] = nums[j];
+            nums[j] = temp;
         }            
         // subarray nums[i .. N-1] is in reverse order
         // using two pointer (begin from both sides) is clearer and easier than the following mechanism
